7c76628ac43e90c390c7d644f39ef5078ffdcf3c
leokwanbt14 <leokwanbt14@gmail.com>
2017-11-17 02:54:35 -0500

    fix

---
+++ BaseExerciseCatalog.ts
@@ -0,0 +1,30 @@
+import BaseWorkoutCatalog from './BaseWorkoutCatalog';
+import BaseExerciseDefinition from './BaseExerciseDefinition';
+import ExerciseBlock from './ExerciseBlock';
+import ExerciseGoal from './ExerciseGoal';
+
+class BaseExerciseCatalog {
+  id: number;
+  protected block: string;
+  getBlock(): ExerciseBlock {
+    return ExerciseBlock[this.block]
+  }
+  setBlock(block: ExerciseBlock) {
+    this.block = ExerciseBlock[block]
+  }
+
+  goal: ExerciseGoal;
+  sets: number;
+  rpe: number;
+  blockOrder: number;
+  priority: number;
+  reps?: number;
+  manualWeight?: number;
+  percentBodyweight?: number;
+  percentMaxWeight: number;
+  duration?: number;
+  workoutCatalog: BaseWorkoutCatalog;
+  definition: BaseExerciseDefinition;
+}
+
+export default BaseExerciseCatalog
