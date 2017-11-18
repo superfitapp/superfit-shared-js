8f534073054f422331642a287af4b94be5ea3baa
leokwanbt14 <leokwanbt14@gmail.com>
2017-11-18 15:17:08 -0500

    wip

--- ProgramModule.d.ts
+++ ProgramModule.d.ts
@@ -1,9 +1,29 @@
-import BaseExerciseDefinition from './BaseExerciseDefinition';
+import { MovementType, MovementPlane, MovementCategory } from './MovementModule';
 import ExerciseBlock from './ExerciseBlock';
 import ExerciseGoal from './ExerciseGoal';
 import WorkoutType from './WorkoutType';
 import Level from './Level';
 import BaseCoach from './BaseCoach';
+export declare class IProgramCatalog {
+    id: number;
+    title: string;
+    sport: string;
+    summary: string;
+    numberOfWeeks: number;
+    tagline: string;
+    level: string;
+    revisionDate: Date;
+    publishDate: Date;
+    isLive: boolean;
+    creationDate: Date;
+    seriesOrder: number;
+    slug: string;
+    version: number;
+    coach: BaseCoach;
+    workoutCatalogs: IWorkoutCatalog[];
+    getLevel(): Level;
+    setLevel(level: Level): void;
+}
 export declare class BaseProgramCatalog {
     id: number;
     title: string;
@@ -11,7 +31,7 @@ export declare class BaseProgramCatalog {
     summary: string;
     numberOfWeeks: number;
     tagline: string;
-    protected level: string;
+    level: string;
     revisionDate: Date;
     publishDate: Date;
     isLive: boolean;
@@ -24,19 +44,43 @@ export declare class BaseProgramCatalog {
     getLevel(): Level;
     setLevel(level: Level): void;
 }
+export interface IWorkoutCatalog {
+    id: number;
+    slug: string;
+    title: string;
+    type: string;
+    programCatalog?: IProgramCatalog;
+    exerciseCatalogs: IExerciseCatalog[];
+}
 export declare class BaseWorkoutCatalog {
     id: number;
     slug: string;
     title: string;
-    protected type: string;
+    type: string;
     programCatalog?: BaseProgramCatalog;
     exerciseCatalogs: BaseExerciseCatalog[];
     getType(): WorkoutType;
     setType(type: WorkoutType): void;
 }
+export interface IExerciseCatalog {
+    id: number;
+    block: string;
+    goal: ExerciseGoal;
+    sets: number;
+    rpe: number;
+    blockOrder: number;
+    priority: number;
+    reps?: number;
+    manualWeight?: number;
+    percentBodyweight?: number;
+    percentMaxWeight: number;
+    duration?: number;
+    workoutCatalog: IWorkoutCatalog;
+    definition: IExerciseDefinition;
+}
 export declare class BaseExerciseCatalog {
     id: number;
-    protected block: string;
+    block: string;
     getBlock(): ExerciseBlock;
     setBlock(block: ExerciseBlock): void;
     goal: ExerciseGoal;
@@ -51,4 +95,34 @@ export declare class BaseExerciseCatalog {
     duration?: number;
     workoutCatalog: BaseWorkoutCatalog;
     definition: BaseExerciseDefinition;
+}
+export interface IExerciseDefinition {
+    id: number;
+    title: string;
+    slug: string;
+    movementType: string;
+    category: string;
+    plane: string;
+    isBodyweight: boolean;
+    athleticIndex: number;
+    demoUrl: string;
+    unilateral: boolean;
+}
+export declare class BaseExerciseDefinition {
+    id: number;
+    title: string;
+    slug: string;
+    movementType: string;
+    category: string;
+    plane: string;
+    isBodyweight: boolean;
+    athleticIndex: number;
+    demoUrl: string;
+    unilateral: boolean;
+    getBlock(): MovementType;
+    setBlock(movementType: MovementType): void;
+    getPlane(): MovementPlane;
+    setPlane(plane: MovementPlane): void;
+    getCategory(): MovementCategory;
+    setCategory(category: MovementCategory): void;
 }
--- ProgramModule.js
+++ ProgramModule.js
@@ -1,8 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const MovementModule_1 = require("./MovementModule");
 const ExerciseBlock_1 = require("./ExerciseBlock");
 const WorkoutType_1 = require("./WorkoutType");
 const Level_1 = require("./Level");
+class IProgramCatalog {
+    getLevel() {
+        return Level_1.default[this.level];
+    }
+    setLevel(level) {
+        this.level = Level_1.default[level];
+    }
+}
+exports.IProgramCatalog = IProgramCatalog;
 class BaseProgramCatalog {
     getLevel() {
         return Level_1.default[this.level];
@@ -30,3 +40,24 @@ class BaseExerciseCatalog {
     }
 }
 exports.BaseExerciseCatalog = BaseExerciseCatalog;
+class BaseExerciseDefinition {
+    getBlock() {
+        return MovementModule_1.MovementType[this.movementType];
+    }
+    setBlock(movementType) {
+        this.movementType = MovementModule_1.MovementType[movementType];
+    }
+    getPlane() {
+        return MovementModule_1.MovementPlane[this.plane];
+    }
+    setPlane(plane) {
+        this.plane = MovementModule_1.MovementPlane[plane];
+    }
+    getCategory() {
+        return MovementModule_1.MovementCategory[this.category];
+    }
+    setCategory(category) {
+        this.category = MovementModule_1.MovementCategory[category];
+    }
+}
+exports.BaseExerciseDefinition = BaseExerciseDefinition;
