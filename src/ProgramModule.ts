ef32cff93240da82861053f23b4672e0fbd1ac0b
leokwanbt14 <leokwanbt14@gmail.com>
2017-11-18 15:15:40 -0500

    wip

--- src/ProgramModule.ts
+++ src/ProgramModule.ts
@@ -1,9 +1,35 @@
-import BaseExerciseDefinition from './BaseExerciseDefinition';
+import { MovementType, MovementPlane, MovementCategory } from './MovementModule';
 import ExerciseBlock from './ExerciseBlock';
 import ExerciseGoal from './ExerciseGoal';
 import WorkoutType from './WorkoutType'
 import Level from './Level'
 import BaseCoach from './BaseCoach'
+
+export class IProgramCatalog {
+  id: number;
+  title: string;
+  sport: string;
+  summary: string;
+  numberOfWeeks: number;
+  tagline: string;
+  level: string;
+  revisionDate: Date;
+  publishDate: Date;
+  isLive: boolean;
+  creationDate: Date;
+  seriesOrder: number;
+  slug: string;
+  version: number;
+  coach: BaseCoach;
+  workoutCatalogs: IWorkoutCatalog[]
+
+  getLevel(): Level {
+    return Level[this.level]
+  }
+  setLevel(level: Level) {
+    this.level = Level[level]
+  }
+}
 
 export class BaseProgramCatalog {
   id: number;
@@ -12,7 +38,7 @@ export class BaseProgramCatalog {
   summary: string;
   numberOfWeeks: number;
   tagline: string;
-  protected level: string;
+  level: string;
   revisionDate: Date;
   publishDate: Date;
   isLive: boolean;
@@ -31,11 +57,22 @@ export class BaseProgramCatalog {
   }
 }
 
+
+export interface IWorkoutCatalog {
+  id: number;
+  slug: string;
+  title: string;
+  type: string;
+  programCatalog?: IProgramCatalog;
+  exerciseCatalogs: IExerciseCatalog[]
+}
+
+
 export class BaseWorkoutCatalog {
   id: number;
   slug: string;
   title: string;
-  protected type: string;
+  type: string;
   programCatalog?: BaseProgramCatalog;
   exerciseCatalogs: BaseExerciseCatalog[]
 
@@ -48,9 +85,26 @@ export class BaseWorkoutCatalog {
   }
 }
 
+export interface IExerciseCatalog {
+  id: number;
+  block: string;
+  goal: ExerciseGoal;
+  sets: number;
+  rpe: number;
+  blockOrder: number;
+  priority: number;
+  reps?: number;
+  manualWeight?: number;
+  percentBodyweight?: number;
+  percentMaxWeight: number;
+  duration?: number;
+  workoutCatalog: IWorkoutCatalog;
+  definition: IExerciseDefinition;
+}
+
 export class BaseExerciseCatalog {
   id: number;
-  protected block: string;
+  block: string;
   getBlock(): ExerciseBlock {
     return ExerciseBlock[this.block]
   }
@@ -70,4 +124,53 @@ export class BaseExerciseCatalog {
   duration?: number;
   workoutCatalog: BaseWorkoutCatalog;
   definition: BaseExerciseDefinition;
+}
+
+export interface IExerciseDefinition {
+  id: number;
+  title: string;
+  slug: string;
+  movementType: string
+  category: string;
+  plane: string;
+  isBodyweight: boolean;
+  athleticIndex: number;
+  demoUrl: string;
+  unilateral: boolean
+}
+
+export class BaseExerciseDefinition {
+  id: number;
+  title: string;
+  slug: string;
+  movementType: string
+
+  category: string;
+
+  plane: string;
+  isBodyweight: boolean;
+  athleticIndex: number;
+  demoUrl: string;
+  unilateral: boolean
+
+  getBlock(): MovementType {
+    return MovementType[this.movementType]
+  }
+  setBlock(movementType: MovementType) {
+    this.movementType = MovementType[movementType]
+  }
+
+  getPlane(): MovementPlane {
+    return MovementPlane[this.plane]
+  }
+  setPlane(plane: MovementPlane) {
+    this.plane = MovementPlane[plane]
+  }
+
+  getCategory(): MovementCategory {
+    return MovementCategory[this.category]
+  }
+  setCategory(category: MovementCategory) {
+    this.category = MovementCategory[category]
+  }
 }
