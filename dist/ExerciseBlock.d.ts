43e0b86be5f374a16fab6238939abab936f2349e
Leo Kwan <leojwak@gmail.com>
2019-03-23 15:32:43 -0400

    updating library

--- dist/ExerciseBlock.d.ts
+++
@@ -1,7 +0,0 @@
-export default class ExerciseBlock2 {
-    static Warmup: string;
-    static Drills: string;
-    static Cooldown: string;
-    static PowerStrength: string;
-    static Conditioning: string;
-}
