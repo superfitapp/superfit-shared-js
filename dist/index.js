a256b65d6c80efe27c15773b32e1f13591a988c0
Leo Kwan <leojwak@gmail.com>
2020-03-17 00:15:14 -0400

    add peer deps again

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var uom_1 = require("uom");
-var lodash_round_1 = __importDefault(require("lodash.round"));
+import { Amount, Units } from "uom";
+import round from 'lodash.round';
 2;
-exports.convertedWeight = function (unitToConvertFrom, unitToConvertTo, weight, levelsOfPrecision) {
+export var convertedWeight = function (unitToConvertFrom, unitToConvertTo, weight, levelsOfPrecision) {
     if (levelsOfPrecision === void 0) { levelsOfPrecision = 1; }
     if (!weight) {
         return null;
     }
-    var unit = (unitToConvertFrom == "kilogram" /* Kilogram */ ? uom_1.Units.Kilogram : uom_1.Units.PoundLb);
-    var amount = uom_1.Amount.create(weight, unit);
+    var unit = (unitToConvertFrom == "kilogram" /* Kilogram */ ? Units.Kilogram : Units.PoundLb);
+    var amount = Amount.create(weight, unit);
     if (unitToConvertTo == "kilogram" /* Kilogram */) {
-        var toKilos = uom_1.Amount.valueAs(uom_1.Units.Kilogram, amount);
-        return lodash_round_1.default(toKilos, levelsOfPrecision);
+        var toKilos = Amount.valueAs(Units.Kilogram, amount);
+        return round(toKilos, levelsOfPrecision);
     }
     else {
-        var toPounds = uom_1.Amount.valueAs(uom_1.Units.PoundLb, amount);
-        return lodash_round_1.default(toPounds, levelsOfPrecision);
+        var toPounds = Amount.valueAs(Units.PoundLb, amount);
+        return round(toPounds, levelsOfPrecision);
     }
 };
