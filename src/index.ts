2a1b80504ae263eafb4b2e265c949eca110cd805
Leo Kwan <leojwak@gmail.com>
2020-04-23 01:24:56 -0400

    add section order to exercise

--- src/index.ts
+++ src/index.ts
@@ -192,6 +192,7 @@ export interface IExercise_Response_V1 {
   revisionDate: Date;
   title: string;
   exerciseTemplateId: string;
+  sectionOrder: number;
   blockOrder: number;
   priority: number;
   block: string;
